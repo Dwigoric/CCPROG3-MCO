b5845741ed0cac20a149f4bbd8517fb5aad52b02
Joshua Permito <dwigoric@insanedevs.com>
2022-10-15 22:28:48 +0800

    src: make Driver class and some empty methods

--- src/Player.java
+++ src/Player.java
@@ -1,19 +1,61 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Player {
     private float exp = 0;
     private int level = 0;
     private int typeLevel = 0;
-    private int Objectcoins = 100;
+    private int objectCoins = 100;
+    private Farm farm;
 
-    private String typeName[] = {"Farmer", "Registered Farmer",
-                                 "Distinguished Farmer", "Legendary Farmer"};
-
-    public Player() {}
+    private ArrayList<String> typeName = new ArrayList<String>(Arrays.asList(
+            "Farmer",
+            "Registered Farmer",
+            "Distinguished Farmer",
+            "Legendary Farmer"
+    ));
 
     public void addExp(float amount) {
-        this.exp+= amount;
+        this.exp += amount;
     }
 
     public void upgradeType() {
         this.typeLevel++;
+    }
+
+    public void buySeeds() {
+
+    }
+
+    public void advanceDay() {
+
+    }
+
+    public void plantSeed(String cropType) {
+
+    }
+
+    public boolean harvestCrop(int row, int column) {
+        return false;
+    }
+
+    public void waterCrop(int row, int column) {
+
+    }
+
+    public void plowTile(int row, int column) {
+
+    }
+
+    public void shovelTile(int row, int column) {
+
+    }
+
+    public void pickaxeTile(int row, int column) {
+
+    }
+
+    public int getCoins() {
+        return objectCoins;
     }
 }
