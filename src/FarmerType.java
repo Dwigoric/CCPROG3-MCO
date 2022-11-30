177eda2aa47a2a4bbb9108bdd3dced84af38b65a
Joshua Permito <dwigoric@insanedevs.com>
2022-12-01 06:55:46 +0800

    src: remove unnecessary constructors for records

--- src/FarmerType.java
+++ src/FarmerType.java
@@ -1,19 +1,12 @@
 /**
  * The FarmerType record.
+ * @param typeName                     The name of the farmer type.
+ * @param levelRequirement             The level requirement of the farmer type.
+ * @param bonusEarnings                The bonus earnings of the farmer type.
+ * @param seedCostReduction            The seed cost reduction of the farmer type.
+ * @param waterBonusLimitIncrease      The water bonus limit increase of the farmer type.
+ * @param fertilizerBonusLimitIncrease The fertilizer bonus limit increase of the farmer type.
+ * @param registrationFee              The registration fee of the farmer type.
  */
 public record FarmerType(String typeName, int levelRequirement, int bonusEarnings, int seedCostReduction,
-                         int waterBonusLimitIncrease, int fertilizerBonusLimitIncrease, int registrationFee) {
-    /**
-     * The FarmerType class constructor.
-     *
-     * @param typeName                     The name of the farmer type.
-     * @param levelRequirement             The level requirement of the farmer type.
-     * @param bonusEarnings                The bonus earnings of the farmer type.
-     * @param seedCostReduction            The seed cost reduction of the farmer type.
-     * @param waterBonusLimitIncrease      The water bonus limit increase of the farmer type.
-     * @param fertilizerBonusLimitIncrease The fertilizer bonus limit increase of the farmer type.
-     * @param registrationFee              The registration fee of the farmer type.
-     */
-    public FarmerType {
-    }
-}
+                         int waterBonusLimitIncrease, int fertilizerBonusLimitIncrease, int registrationFee) {}
--- src/Seed.java
+++ src/Seed.java
@@ -1,24 +1,17 @@
 /**
  * The Seed record.
+ * @param name             The name of the seed.
+ * @param harvestTime      The harvest time of the seed.
+ * @param waterNeeds       The minimum water level of the seed.
+ * @param fertilizerNeeds  The minimum fertilizer level of the seed.
+ * @param minProduce       The minimum produce of the seed.
+ * @param maxProduce       The maximum produce of the seed.
+ * @param cost             The cost of the seed.
+ * @param baseSellingPrice The base value of the seed.
+ * @param expYield         The experience gained from harvest the crop from this seed.
+ * @param isFlower         Whether the seed is a flower.
+ * @param isTree           Whether the seed is a tree.
  */
 public record Seed(String name, int harvestTime, int waterNeeds, int waterLimit, int fertilizerNeeds,
                    int fertilizerLimit, int minProduce, int maxProduce, int cost, int baseSellingPrice, float expYield,
-                   boolean isFlower, boolean isTree) {
-    /**
-     * The Seed class constructor.
-     *
-     * @param name             The name of the seed.
-     * @param harvestTime      The harvest time of the seed.
-     * @param waterNeeds       The minimum water level of the seed.
-     * @param fertilizerNeeds  The minimum fertilizer level of the seed.
-     * @param minProduce       The minimum produce of the seed.
-     * @param maxProduce       The maximum produce of the seed.
-     * @param cost             The cost of the seed.
-     * @param baseSellingPrice The base value of the seed.
-     * @param expYield         The experience gained from harvest the crop from this seed.
-     * @param isFlower         Whether the seed is a flower.
-     * @param isTree           Whether the seed is a tree.
-     */
-    public Seed {
-    }
-}
+                   boolean isFlower, boolean isTree) {}
